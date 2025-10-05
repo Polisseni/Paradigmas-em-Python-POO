cce91d11d1ce280ae3bd81d78a048271e41c6181
Vitor Nogueres Polisseni <vitorpolisseni68@gmail.com>
2025-10-05 16:50:50 -0300

    Analisando Textos Em Python

---
+++ Analisador De Textos.py
@@ -0,0 +1,8 @@
+nome = str(input('Digite seu nome: ')).strip()
+print('ANALISANDO SEU NOME...')
+print(f'Seu nome escrito em letras maiúsculas é {nome.upper()}.')
+print(f'Seu nome escrito em letras minúsculas é {nome.lower()}.')
+print(f'Seu nome completo tem ao todo {len(nome) - nome.count(' ')} letras.')
+#print(f'Seu primeiro nome tem {nome.find(' ')} letras.')
+separa = nome.split()
+print(f'Seu primeiro nome é {separa[0]} e ele tem {len(separa[0])} letras.')
