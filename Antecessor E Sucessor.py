c7fb1283e4425cff76f4bc74a5ef6f4d14b4e28a
Vitor Nogueres Polisseni <vitorpolisseni68@gmail.com>
2025-10-05 16:52:10 -0300

    Antecessor E Sucessor

---
+++ Antecessor E Sucessor.py
@@ -0,0 +1,6 @@
+n = int(input('Digite um número inteiro: '))
+a = n - 1
+s = n + 1
+print(f'O número digitado foi {n} e seu antecessor é {a} e seu sucessor é {s}')
+'''n = int(input('Digite um número inteiro: '))
+print(f'O número digitado foi {n} e seu antecessor é {n-1} e seu sucessor é {n+1}')'''
