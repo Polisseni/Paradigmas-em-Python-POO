3355b368c7abeeb9930055f7a51debf150528621
Vitor Nogueres Polisseni <vitorpolisseni68@gmail.com>
2025-10-08 17:05:51 -0300

    Mexendo Com Dobro, Triplo E Raiz Quadrada

---
+++ Dobro Triplo E Raiz Quadrada.py
@@ -0,0 +1,7 @@
+n = int(input('Digite um número: '))
+d = n * 2
+t = n * 3
+r = n ** (1/2)
+print(f'O número digitado foi {n} e seu dobro é {d}, seu triplo {t} e sua raiz quadrada é igual a {r:.3f}')
+'''n = int(input('Digite um número: '))
+print(f'O número digitado foi {n} e seu dobro é {n*2}, seu triplo {n*3} e sua raiz quadrada é igual a {n**(1/2):.3f}')'''
