44c2447366abb9c344daff0bf8ce6b296e32ce9e
Vitor Nogueres Polisseni <vitorpolisseni68@gmail.com>
2025-10-15 16:25:04 -0300

    class Extrato

---
+++ classes/Extrato.py
@@ -0,0 +1,12 @@
+class Extrato:
+
+    def __init__(self):
+        self.transaçoes = []
+
+    def gerar_extrato(self, conta):
+        print(f"\033[34mExtrato da conta {conta}\033[m")
+        for tran in self.transaçoes:
+            print(f"{tran[0]:15s} {tran[1]:10.2f} {tran[2].strftime('%d/%b/%Y')}")
+
+
+# [0 - Tipo 1 - Tipo 2 - Data]
