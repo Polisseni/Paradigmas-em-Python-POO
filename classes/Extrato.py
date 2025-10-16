853d9eaeda4a6c84c8c6f0a6e607f8e7c41b04ce
Vitor Nogueres Polisseni <vitorpolisseni68@gmail.com>
2025-10-16 17:13:55 -0300

    class Extrato(MODIFICAÇÃO)

--- classes/Extrato.py
+++ classes/Extrato.py
@@ -6,7 +6,9 @@ class Extrato:
     def gerar_extrato(self, conta):
         print(f"\033[34mExtrato da conta {conta}\033[m")
         for tran in self.transaçoes:
-            print(f"{tran[0]:15s} {tran[1]:10.2f} {tran[2].strftime('%d/%b/%Y')}")
+            print(f"{tran[0]:15s} {tran[1]:12.2f} {tran[2].strftime('%d/%b/%Y')}")
+
+        print(f"SALDO ATUAL:R${conta.saldo:12.2f} \n")
 
 
 # [0 - Tipo 1 - Tipo 2 - Data]
