fe4eb49d26318462d873397bf04317305a947c87
Vitor Nogueres Polisseni <vitorpolisseni68@gmail.com>
2025-10-17 15:41:55 -0300

    Numero Por Extenso

---
+++ Numero Por Extenso.py
@@ -0,0 +1,9 @@
+contagem = ('zero' , 'um' , 'dois' , 'três' , 'quatro' , 'cinco' , 'seis' ,
+            'sete' , 'oito' , 'nove' , 'dez' , 'onze' , 'doze' , 'treze' ,
+            'quatorze' , 'quinze' , 'dezesseis' , 'dezessete' , 'dezoito' , 'dezenove' , 'vinte')
+while True:
+    num = int(input('Digite um número entre 0 e 20: '))
+    if 0 <= num <= 20:
+        break
+    print('TENTE NOVAMENTE!' , end = ' ')
+print(f'Você digitou o número {contagem[num]}')
