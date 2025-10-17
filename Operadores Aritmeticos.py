1591d94a2b4d603a88c1b3e59fa5dbd55744b939
Vitor Nogueres Polisseni <vitorpolisseni68@gmail.com>
2025-10-17 15:42:23 -0300

    Operadores Aritmeticos Em Python

---
+++ Operadores Aritmeticos.py
@@ -0,0 +1,9 @@
+n1 = int(input('Digite um número inteiro: '))
+n2 = int(input('Digite outro número inteiro: '))
+s = n1 + n2
+m = n1 * n2
+d = n1 / n2
+di = n1 // n2
+e = n1 ** n2
+print(f'A soma entre os números inteiros é igual a {s}, a multiplicação entre eles é {m} e a divisão é igual a {d:.3f}')
+print(f'O resultado da divisão inteira entre eles é {di} e a potência é igual a {e}')
