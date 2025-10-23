c59982e84f9bdd7b1642f3a8e61ecc34bef313d5
Vitor Nogueres Polisseni <vitorpolisseni68@gmail.com>
2025-10-23 19:17:13 -0300

    Varios Numeros Flag

---
+++ Varios Numeros Com Flag.py
@@ -0,0 +1,8 @@
+c = s = 0
+while True:
+    n = int(input('Digite um número (999 parar): '))
+    if n == 999:
+        break
+    c += 1
+    s += n
+print(f'Foram digitados {c} números e a soma deles é igual a {s}!')
